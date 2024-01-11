0d8d67efd7cd1443b2bce918d6e32d1501cb9fb9
DaniloOne <dcg070363@gmail.com>
2024-01-11 11:05:49 +0100

    What I did in 30 minutes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*TO-DO
--Input validation
+-Input validation(With Exceptions)
 */
 include('files/students.php');
 include('files/activities.php');
@@ -12,14 +12,20 @@ function showOptions(array $activities): void {
 	}
 }
 
+function checkOptionInput(int $activities_count,string $option): bool {
+	return is_int($option) && $option > 0 && $ 
+}
+
 function getRandomStudent(array $students): int {
 	return rand(0,count($students)-1);
 }
 
 showOptions($activities);
 $activity_index = readline("Hei! Quina activitat vols assignar?(Introdueix la opció numèrica)");
-$student_index = getRandomStudent($students);
 
-echo "Li toca a ".$students[$student_index]." fer ".$activities[intval($activity_index)-1]." felicitats!!"; 
+if(checkOptionInput(count($activities),$activity_index) {
+	$student_index = getRandomStudent($students);
+	echo "Li toca a ".$students[$student_index]." fer ".$activities[($activity_index)-1]." felicitats!!"; 
+}
 
 ?>
