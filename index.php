0791eb950faf8098951775da0eb093169ac5bb3f
DaniloOne <dcg070363@gmail.com>
2024-01-11 10:43:52 +0100

    Added Students Generator

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include('files/students.php');
+
+var_dump($students);
+
+/*
+function mostrarActividades(): void {
+
+
+}
+
+mostrarActividades();
+$option = readline("Hei! Qué actividad quieres asignar?")
+*/
+
+?>
